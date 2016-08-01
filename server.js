b163a1d61ddf7b303ff16a9ccb028ee8edd3d6ca
wajidmehraj <wajidmehraj@msn.com>
2016-08-01 21:19:21 +0530

    Checking commit with webstorm

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/',function(req,resp){
-    resp.send('Hello my two jigggus -- nanuuu ji and mehvi juiiiii');
+    resp.send('Hello my two jigggus -- nanuuu ji and mehvi juiiiii. Adnanu is doing good . He is such a nice boy');
 });
 
 app.listen(app.get('port'),function(){
